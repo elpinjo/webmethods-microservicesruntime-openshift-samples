17c9646004bc9322f6e58b2f883310e1daa3eeee
Heuveling <mihe@softwareag.com>
2019-11-12 00:05:24 +0100

    updated project path

--- Build.sh
+++ Build.sh
@@ -2,8 +2,8 @@
 #
 
 docker build -t microruntime:v2 -f Dockerfile .
-docker tag microruntime:v2 172.30.1.1:5000/msrnamespace/microruntime:v2
+docker tag microruntime:v2 172.30.1.1:5000/samplepipeline/microruntime:v2
 oc login -u developer -p manage
-oc project msrnamespace
+oc project samplepipeline
 docker login -u developer -p $(oc whoami -t) 172.30.1.1:5000
-docker push 172.30.1.1:5000/msrnamespace/microruntime:v2
+docker push 172.30.1.1:5000/samplepipeline/microruntime:v2
